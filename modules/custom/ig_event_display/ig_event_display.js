b3e9268cb02cdebd0b6df1255b725f0b964bfcfe
Arvin Singla <arvin.singla@gmail.com>
2015-07-29 09:57:09 -0400

    Initial test for fetching and displaying images from Angular with angular animate.

--- modules/custom/ig_event_display/ig_event_display.js
+++
@@ -1,7 +0,0 @@
-/**
- * Render the event display.
- */
-var app = angular.module('eventApp', [])
-    .controller('eventController', function($scope) {
-        $scope.title = Drupal.settings.igEventDisplay.title;
-    });
---
+++ modules/custom/ig_event_display/js/ig_event_display.js
@@ -0,0 +1,31 @@
+/**
+ * Render the event display.
+ */
+angular.module('eventApp', ['ngAnimate'])
+.controller('eventController', function($http, $scope, $timeout) {
+
+    var eventData = Drupal.settings.igEventDisplay,
+        initialFetch = true;
+
+    var rotate = function () {
+        if ($scope.items && $scope.items.length) {
+            $scope.currentItemIndex = ($scope.currentItemIndex >= $scope.items.length - 1) ? 0 : ++$scope.currentItemIndex;
+        }
+        $timeout(rotate, 5000);
+    };
+
+    $scope.title = Drupal.settings.igEventDisplay.title;
+    $scope.hashtag = Drupal.settings.igEventDisplay.hashtag;
+    $scope.currentItemIndex = 0;
+
+    $http.get(eventData.url + '/api/event/' + eventData.nid + '/items').
+        success(function(data) {
+            $scope.items = data;
+            if (initialFetch) {
+                initialFetch = false;
+                rotate();
+            }
+        })
+        .error(function(error) {
+        });
+});
